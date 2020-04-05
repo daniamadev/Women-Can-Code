e23c9ac6c78052beef6d3edee90f051a9aacce18
Daniela Amadeu <danielapamadeu@gmail.com>
2020-04-05 17:50:29 -0300

    stories e user loop

--- aula-23/instagram-clone/src/components/Sidebar/index.js
+++ aula-23/instagram-clone/src/components/Sidebar/index.js
@@ -1,32 +1,64 @@
-import React from "react";
+import React, { Component } from "react"
+import axios from "axios"
 
-export default function Sidebar() {
-  return (
-    <div>
-      <div className="user-info">
-        <img
-          alt="Perfil"
-          src="https://randomuser.me/api/portraits/women/25.jpg"
-        />
-        <div className="user-bio">
-          <strong>Jeniffer</strong>
-          <span>jenicarvalho</span>
+//export default function Sidebar() {
+class Sidebar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {}, //user é um objeto
+      stories: [], //é uma array porque são vários itens
+    }
+  }
+
+  async getStories() {
+    const { data } = await axios.get("http://localhost:3000/stories")
+    this.setState({ stories: data })
+  }
+
+  async getUser() {
+    const { data } = await axios.get("http://localhost:3000/user")
+    this.setState({ user: data })
+  }
+
+  componentDidMount = () => {
+    // axios.get("http://localhost:3000/stories").then((res) => {
+    //   this.setState({ conteudo: res.data })
+    // })
+    this.getStories()
+    this.getUser()
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="user-info">
+          <img
+            alt="Perfil"
+            src="https://randomuser.me/api/portraits/women/25.jpg"
+          />
+          <div className="user-bio">
+            <strong>{this.state.user.name}</strong>
+            <span>jenicarvalho</span>
+          </div>
         </div>
-      </div>
-
-      <div className="stories">
         <h2>Stories</h2>
-
-        <div className="storie">
-          <div className="storie-image">
-            <img src="https://randomuser.me/api/portraits/men/11.jpg" alt="user" />
+        {this.state.stories.map((item) => (
+          <div key={item.id} className="stories">
+            <div className="storie">
+              <div className="storie-image">
+                <img src={item.userPicture} alt="user" />
+              </div>
+              <div className="storie-user">
+                <strong>{item.user}</strong>
+                <span>{item.time}</span>
+              </div>
+            </div>
           </div>
-          <div className="storie-user">
-            <strong>history</strong>
-            <span>há 2 horas</span>
-          </div>
-        </div>
+        ))}
       </div>
-    </div>
-  );
+    )
+  }
 }
+
+export default Sidebar
--- aula-23/instagram-clone/src/components/TimeLine/index.js
+++ aula-23/instagram-clone/src/components/TimeLine/index.js
@@ -1,53 +1,52 @@
 import React, { Component } from "react";
-import { FiHeart } from 'react-icons/fi'
-import axios from 'axios';
+import { FiHeart } from "react-icons/fi";
+import axios from "axios";
 import Helmet from "react-helmet";
 
 //primeiro arquivo a ser trabalhado
 
 //export default function TimeLine() {
-  class Timeline extends Component {
+class Timeline extends Component {
   state = {
-    conteudo: []
-  }
+    conteudo: [],
+  };
   //tem que ser uma array[]
   //nao um objeto {}
 
   componentDidMount = () => {
-    axios.get("http://localhost:3000/posts")
-      .then(res => {
-        this.setState({ conteudo: res.data })
-      }) 
-  }
-
+    axios.get("http://localhost:3000/posts").then((res) => {
+      this.setState({ conteudo: res.data });
+    });
+  };
 
   render() {
-   return(
+    return (
       <>
-      <Helmet><title>Timeline</title></Helmet>
-      <div className="post">
-        {this.state.conteudo.map(content => (
-          <div key={content.id}>
-            <header>
-              <img src={content.userPicture} alt="Lala"/>
-              <div className="post-user">
-                <strong>{content.user}</strong>
-                <span>{content.location}</span>
+        <Helmet>
+          <title>Timeline</title>
+        </Helmet>
+        <div className="post">
+          {this.state.conteudo.map((content) => (
+            <div key={content.id}>
+              <header>
+                <img src={content.userPicture} alt="Lala" />
+                <div className="post-user">
+                  <strong>{content.user}</strong>
+                  <span>{content.location}</span>
+                </div>
+              </header>
+              <div className="post-image">
+                <img src={content.postPicture} alt="daniela" />
               </div>
-            </header>
-           <div className="post-image">
-            <img src={content.postPicture} alt="daniela" />
-          </div>
-         <div className="post-likes">
-           <FiHeart />
-         </div>
-         <p>{content.description}</p>
-         </div>
-        ))
-        }
-    </div>
-    </>
-    )
+              <div className="post-likes">
+                <FiHeart />
+              </div>
+              <p>{content.description}</p>
+            </div>
+          ))}
+        </div>
+      </>
+    );
   }
 }
 
